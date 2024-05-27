02ba0406311c24bb5c05709738ab572d1a41cab8
Amaan-Majid <amaan.majid@klipit.co>
2024-05-27 16:16:32 +0400

    fix

--- api/routes/statistic.route.js
+++ api/routes/statistic.route.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {
-  viewTotalSales,
-  viewTotalSalesAmount,
-  addInvoice,
-} = require("../controllers/statistic.controller");
-const {
   addNewInvoice,
   viewInvoices,
 } = require("../controllers/newStats.controller");
-router.post("/invoice", addInvoice);
-router.post("/sales", viewTotalSales);
-router.post("/amount", viewTotalSalesAmount);
 
 router.post("/new/invoice", addNewInvoice);
 router.post("/new/get", viewInvoices);
